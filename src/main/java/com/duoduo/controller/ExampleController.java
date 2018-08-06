3ed2b5ac7d98a2b93352d252a22e56c6edd3a6f3
fenghaikuan <fenghaikuan@xiangshang360.com>
2018-08-06 18:49:41 +0800

    init

--- src/main/java/com/duoduo/controller/ExampleController.java
+++ src/main/java/com/duoduo/controller/ExampleController.java
@@ -3,23 +3,21 @@ package com.duoduo.controller;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.duoduo.dao.UserMapper;
+import com.duoduo.dao.UserDao;
 import com.duoduo.entity.User;
-import com.duoduo.entity.UserExample;
 
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
-	@Resource UserMapper userMapper;
+	@Resource UserDao userDao;
 	@RequestMapping(value="index",method=RequestMethod.GET)
 	public String  index(){
-		User user =  userMapper.selectByPrimaryKey(new Integer(1));
+		User user =  userDao.selectByPrimaryKey(new Integer(1));
 		System.out.println(JSON.toJSON(user));
 		System.out.println(user.getPassword());
 		return "example";
@@ -27,7 +25,7 @@ public class ExampleController {
 	@RequestMapping(value="json",method=RequestMethod.GET)
 	@ResponseBody()
 	public String json(){
-		User user =  userMapper.selectByPrimaryKey(new Integer(1));
+		User user =  userDao.selectByPrimaryKey(new Integer(1));
 		System.out.println(JSON.toJSON(user));
 		String str = JSON.toJSON(user).toString();
 		 return str;
