ce61bcf54201c6f4d9e2bc59b0c504e351e5c46a
fenghaikuan <fenghaikuan@xiangshang360.com>
2018-08-16 17:08:46 +0800

    rediskey 修改

--- src/main/java/com/duoduo/conf/RedisKey.java
+++ src/main/java/com/duoduo/conf/RedisKey.java
@@ -1,16 +1,24 @@
 package com.duoduo.conf;
+/**
+ * rediskey 预设配置
+ * @author haikuan
+ *
+ */
+
+import com.alibaba.druid.sql.ast.expr.SQLSequenceExpr.Function;
 
 public class RedisKey {
+	private static final String APPNAME = "web";
 	/** ===================================================用户有关信息======================================================== */
 	// user_index对象前缀,后面拼接用户userId
-	public static final String STRING_USER_INDEX_PREFIX = "user_";
-	//hash使用
-	public static final String HASH_USER_INFO_PREFIX = "userinfo_";
-	//list使用
-	public static final String LIST_USER_LIST_PREFIX = "userlist_";
-	//set使用
-	public static final String SET_USER_LIST_PREFIX = "userset_";
-	//zset使用
-	public static final String ZSET_USER_LIST_PREFIX = "userzset_";
-	
+	public static final String STRING_USER_INDEX_PREFIX = APPNAME + "user_";
+	// hash使用
+	public static final String HASH_USER_INFO_PREFIX = APPNAME + "userinfo_";
+	// list使用
+	public static final String LIST_USER_LIST = APPNAME + "userlist_";
+	// set使用
+	public static final String SET_USER_LIST = APPNAME + "userset_";
+	// zset使用
+	public static final String ZSET_USER_LIST = APPNAME + "userzset_";
+
 }
